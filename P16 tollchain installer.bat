434b3d2a5184b8951b8038bfbecedb44e6bb85e0
Ângelo Azevedo <66081804+ang3lo-azevedo@users.noreply.github.com>
2023-03-09 10:46:30 +0000

    Update P16 tollchain installer.bat

--- P16 tollchain installer.bat
+++ P16 tollchain installer.bat
@@ -1,27 +1,15 @@
 cd %USERPROFILE%\AppData\Roaming
 mkdir P16
 cd P16
-curl https://aka.ms/vs/17/release/vc_redist.x86.exe -JLO
-vc_redist.x86.exe updateall -q
-del vc_redist.x86.exe
-curl https://go.microsoft.com/fwlink/?LinkId=2085155 -JLo ndp48-web.exe
-ndp48-web.exe updateall -q
-del ndp48-web.exe
+curl https://aka.ms/vs/17/release/vc_redist.x86.exe -JLO && vc_redist.x86.exe updateall -q && del vc_redist.x86.exe
+curl https://go.microsoft.com/fwlink/?LinkId=2085155 -JLo ndp48-web.exe && ndp48-web.exe updateall -q && del ndp48-web.exe
 mkdir p16as
 mkdir p16dbg
 mkdir p16sim
 cd p16as
-curl https://github.com/ang3lo-azevedo/P16-toolchain-installer/blob/main/files/p16as.zip?raw=true -JLo p16as.zip
-tar -xf p16as.zip
-del p16as.zip
-cd ..
-cd p16dbg
-curl https://github.com/ang3lo-azevedo/P16-toolchain-installer/blob/main/files/p16dbg.zip?raw=true -JLo p16dbg.zip
-tar -xf p16dbg.zip
-del p16dbg.zip
-cd ..
-cd p16sim
-curl https://github.com/ang3lo-azevedo/P16-toolchain-installer/blob/main/files/p16sim.zip?raw=true -JLo p16sim.zip
-tar -xf p16sim.zip
-del p16sim.zip
+curl https://github.com/ang3lo-azevedo/P16-toolchain-installer/blob/main/files/p16as.zip?raw=true -JLo p16as.zip && tar -xf p16as.zip && del p16as.zip
+cd ..\p16dbg
+curl https://github.com/ang3lo-azevedo/P16-toolchain-installer/blob/main/files/p16dbg.zip?raw=true -JLo p16dbg.zip && tar -xf p16dbg.zip && del p16dbg.zip
+cd ..\p16sim
+curl https://github.com/ang3lo-azevedo/P16-toolchain-installer/blob/main/files/p16sim.zip?raw=true -JLo p16sim.zip && tar -xf p16sim.zip && del p16sim.zip
 setx PATH "%USERPROFILE%\AppData\Roaming\P16\p16as;%USERPROFILE%\AppData\Roaming\P16\p16sim;%PATH%"
